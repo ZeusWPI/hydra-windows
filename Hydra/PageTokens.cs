45a99f91a7d42ceae93e6de753210c597a8dc084
Niels De Graef <nielsdegraef@gmail.com>
2015-11-25 03:44:25 +0100

    First try at adding PRISM

---
+++ Hydra/PageTokens.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hydra {
+    public static class PageTokens {
+        public const string HomePage = "Home";
+        public const string RestoPage = "Resto";
+    }
+}
---
+++ Hydra/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Hydra.ViewModels {
+
+    public class MenuItemViewModel : ViewModelBase {
+
+        public string DisplayName { get; set; }
+
+        public Symbol FontIcon { get; set; }
+
+        public ICommand Command { get; set; }
+
+        public override string ToString() {
+            return DisplayName;
+        }
+    }
+}
