8ed5d4dd6bf82d42916112cbcaac65546a0248dd
Niels De Graef <nielsdegraef@gmail.com>
2016-01-28 02:15:51 +0100

    Added urgent.fm

---
+++ Hydra/ViewModels/UrgentFm/UrgentFmPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Hydra.DataSources;
+using Hydra.Models.Schamper;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Hydra.ViewModels.UrgentFm {
+    public class UrgentFmPageViewModel : ViewModelBase, INotifyPropertyChanged {
+
+        public Uri UrgentStreamUrl { get { return new Uri("http://195.10.2.96/urgent/high.mp3"); } }
+
+        public bool Playing { get; set; } = false;
+        public ICommand PlayPauseCommand { get; set; }
+
+        public UrgentFmPageViewModel() {
+            PlayPauseCommand = new DelegateCommand(() => {
+                this.Playing = !this.Playing;
+                OnPropertyChanged();
+            });
+        }
+        
+    }
+}
--- Hydra/Views/UrgentFm/UrgentFmPage.xaml.cs
+++ Hydra/Views/UrgentFm/UrgentFmPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Prism.Windows.Mvvm;
+﻿using Hydra.ViewModels.UrgentFm;
+using Prism.Windows.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -8,16 +10,41 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace Hydra.Views.UrgentFm {
     public sealed partial class UrgentFmPage : SessionStateAwarePage {
+
+        private UrgentFmPageViewModel viewModel;
+
         public UrgentFmPage() {
             InitializeComponent();
+
+            this.viewModel = (UrgentFmPageViewModel)this.DataContext;
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            UrgentPlayer.MediaFailed += UrgentPlayer_MediaFailed;
+            PlayPauseButton.Click += PlayPauseButton_Click;
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e) {
+            if (viewModel.Playing) {
+                UrgentPlayer.Stop();
+                PlayPauseSymbol.Symbol = Symbol.Play;
+            } else {
+                UrgentPlayer.Play();
+                PlayPauseSymbol.Symbol = Symbol.Pause;
+            }
+        }
+
+        private void UrgentPlayer_MediaFailed(object sender, ExceptionRoutedEventArgs e) {
+            // TODO: properly notify user
+            Debug.WriteLine("Something went wrong :( \n");
+            Debug.WriteLine(e.ErrorMessage);
+        }
+        
     }
 }
